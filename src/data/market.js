57135bbb4208c7523c55d802c4c47de5866560fc
Arthur <arthur91021@gmail.com>
2025-03-28 08:43:37 -0700

    added market data from previous project

---
+++ src/data/market.js
@@ -0,0 +1,63 @@
+import apple from "../../public/images/Apple.webp";
+import bread from "../../public/images/bread.jpg";
+import eggs from "../../public/images/eggs.webp";
+import milk from "../../public/images/milk.webp";
+import tomato from "../../public/images/tomato.jpeg";
+
+export const market = [
+  {
+    id: 1,
+    name: "Tomato",
+    price: 0.5,
+    category: "Vegetable",
+    inStock: true,
+    photo: tomato,
+    amountOrdered: 0,
+    emoji: "🍅",
+    inventory: 0,
+  },
+  {
+    id: 2,
+    name: "Apple",
+    price: 0.6,
+    category: "Vegetable",
+    inStock: true,
+    photo: apple,
+    amountOrdered: 0,
+    emoji: "🍎",
+    inventory: 0,
+  },
+  {
+    id: 3,
+    name: "Bread",
+    price: 2,
+    category: "Wheat",
+    inStock: true,
+    photo: bread,
+    amountOrdered: 0,
+    emoji: "🍞",
+    inventory: 0,
+  },
+  {
+    id: 4,
+    name: "Eggs",
+    price: 4,
+    category: "Protein",
+    inStock: true,
+    photo: eggs,
+    amountOrdered: 0,
+    emoji: "🥚",
+    inventory: 0,
+  },
+  {
+    id: 5,
+    name: "Milk",
+    price: 3,
+    category: "Dairy",
+    inStock: true,
+    photo: milk,
+    amountOrdered: 0,
+    emoji: "🥛",
+    inventory: 0,
+  },
+];
